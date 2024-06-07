60ed0a3301731d1e977580b888e13d752113cfac
Stanley Chen <42549053+stanley021@users.noreply.github.com>
2024-06-06 21:57:54 -0400

    database changes

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get '*path', to: 'application#handle_missing_route'
 
 end
